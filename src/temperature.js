b29a60ea2b78009012de70d152cebe89319aaf35
Szymon Wolszczak <szymon.wolszczak@tsh.io>
2020-06-16 21:51:16 +0200

    WIP connect to mongo

--- src/temperature.js
+++ src/temperature.js
@@ -1,10 +1,24 @@
 var W1Temp = require('w1temp');
+var MongoClient = require('mongodb').MongoClient;
 
 W1Temp.getSensor('28-020d92462283').then(function (sensor) {
   var temp = sensor.getTemperature();
   console.log('Actual temp:', (temp), '°C');
   sensor.on('change', function (temp) {
     console.log('Temp changed:', temp, '°C');
+  });
+
+});
+
+W1Temp.getSensor('28-020d92462283').then(function (sensor) {
+  var temp = sensor.getTemperature();
+  MongoClient.connect("mongodb://localhost:27017/", function(err, db) {
+  if(err) { return console.dir(err); }
+  db.collection("temperature").insertOne(temp, function(err, collection){
+    if (err) throw err;
+    console.log("temp saved");
+    db.close();
+  }
   });
 
 });
